e1bcc0f50c0c764fde848d0e30ceb79dd43548f5
benrbowers <benjambowers@gmail.com>
2024-12-04 10:30:02 -0500

    Attempt at day 4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -68,19 +69,19 @@ func main() {
 	}
 
 	// Collect down diagonal lines
-	for row := lineCount - 1; row >= 0; row-- {
-		var diag []byte = []byte{}
-
-		for col := range min(lineCount-row, lineLength) {
-			diag = append(diag, horizontal[row+col][col])
-		}
-		downDiagonal = append(downDiagonal, string(diag))
-	}
-	for col := 1; col < lineLength; col++ {
+	for col := lineLength - 1; col >= 0; col-- {
 		var diag []byte = []byte{}
 
 		for row := range min(lineLength-col, lineCount) {
 			diag = append(diag, horizontal[row][col+row])
+		}
+		downDiagonal = append(downDiagonal, string(diag))
+	}
+	for row := 1; row < lineLength; row++ {
+		var diag []byte = []byte{}
+
+		for col := range min(lineCount-row, lineLength) {
+			diag = append(diag, horizontal[row+col][col])
 		}
 		downDiagonal = append(downDiagonal, string(diag))
 	}
@@ -107,4 +108,94 @@ func main() {
 	}
 
 	fmt.Printf("Occurences of \"XMAS\": %d\n", answer)
+
+	masPattern := regexp.MustCompile("MAS")
+	samPattern := regexp.MustCompile("SAM")
+
+	// Coords for a single 'A' in an x-MAS
+	var upMatches [][]int = [][]int{}
+	var downMatches [][]int = [][]int{}
+
+	for i, line := range upDiagonal {
+		for _, indexes := range masPattern.FindAllStringIndex(line, -1) {
+			position := indexes[0] + 1
+
+			var row int
+			var col int
+
+			if i < lineCount {
+				row = i - position
+				col = position
+			} else {
+				row = lineCount - 1 - position
+				col = i - lineCount + 1
+			}
+
+			upMatches = append(upMatches, []int{row, col})
+		}
+		for _, indexes := range samPattern.FindAllStringIndex(line, -1) {
+			position := indexes[0] + 1
+
+			var row int
+			var col int
+
+			if i < lineCount {
+				row = i - position
+				col = position
+			} else {
+				row = lineCount - 1 - position
+				col = i - lineCount + 1
+			}
+
+			upMatches = append(upMatches, []int{row, col})
+		}
+	}
+
+	for i, line := range downDiagonal {
+		for _, indexes := range masPattern.FindAllStringIndex(line, -1) {
+			position := indexes[0] + 1
+
+			var row int
+			var col int
+
+			if i < lineCount {
+				row = position
+				col = lineLength - 1 - i + position
+			} else {
+				row = (i - lineCount) + 1 + position
+				col = position
+			}
+
+			downMatches = append(downMatches, []int{row, col})
+		}
+		for _, indexes := range samPattern.FindAllStringIndex(line, -1) {
+			position := indexes[0] + 1
+
+			var row int
+			var col int
+
+			if i < lineCount {
+				row = position
+				col = lineLength - 1 - i + position
+			} else {
+				row = (i - lineCount) + 1 + position
+				col = position
+			}
+
+			downMatches = append(downMatches, []int{row, col})
+		}
+	}
+
+	var xMasCount int = 0
+
+	for _, upMatch := range upMatches {
+		for _, downMatch := range downMatches {
+			if upMatch[0] == downMatch[0] && upMatch[1] == downMatch[1] {
+				xMasCount++
+				break
+			}
+		}
+	}
+
+	fmt.Printf("Occurrances of x-Mas: %d\n", xMasCount)
 }
