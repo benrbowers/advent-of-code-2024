fa14809ed593886c3480cabe9e261d9712d0ab9f
benrbowers <benjambowers@gmail.com>
2024-12-06 00:20:58 -0500

    ⭐ Star gained for day 5 part 2

--- day5/day5.go
+++ day5/day5.go
@@ -20,6 +20,29 @@ against rules from the first section
 Sections are separated by new line
 */
 
+func swapValues(slice []string, val1, val2 string) {
+	index1 := slices.Index(slice, val1)
+	index2 := slices.Index(slice, val2)
+
+	if index1 == -1 || index2 == -1 {
+		return
+	}
+
+	slice[index1], slice[index2] = slice[index2], slice[index1]
+}
+
+func isValidSequence(sequence []string, rulesList [][]string) bool {
+	for _, rule := range rulesList {
+		if slices.Contains(sequence, rule[0]) && slices.Contains(sequence, rule[1]) {
+			if slices.Index(sequence, rule[0]) > slices.Index(sequence, rule[1]) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func main() {
 	inputFile, err := os.Open("input.txt")
 	if err != nil {
@@ -30,6 +53,7 @@ func main() {
 
 	var rulesList [][]string = [][]string{}
 	var validSequences [][]string = [][]string{}
+	var invalidSequences [][]string = [][]string{}
 	var isFirstSection bool = true
 
 	scanner := bufio.NewScanner(inputFile)
@@ -52,25 +76,17 @@ func main() {
 			rulesList = append(rulesList, ruleNums)
 		} else {
 			pageNums := strings.Split(line, ",")
-			var sequenceValid bool = true
-
-			for _, rule := range rulesList {
-				if slices.Contains(pageNums, rule[0]) && slices.Contains(pageNums, rule[1]) {
-					if slices.Index(pageNums, rule[0]) > slices.Index(pageNums, rule[1]) {
-						sequenceValid = false
-						break
-					}
-				}
-			}
-
-			if sequenceValid {
+			if isValidSequence(pageNums, rulesList) {
 				validSequences = append(validSequences, pageNums)
+			} else {
+				invalidSequences = append(invalidSequences, pageNums)
 			}
 		}
 	}
 
 	// Sum of middle page numbers
 	var answer int = 0
+	var invalidAnswer int = 0
 
 	for _, sequence := range validSequences {
 		middle := sequence[len(sequence)/2]
@@ -82,6 +98,29 @@ func main() {
 		answer += middleNum
 	}
 
+	for _, sequence := range invalidSequences {
+		for !isValidSequence(sequence, rulesList) {
+			// While sequence is not valid, swap values according to rules
+			for _, rule := range rulesList {
+				if slices.Contains(sequence, rule[0]) && slices.Contains(sequence, rule[1]) {
+					if slices.Index(sequence, rule[0]) > slices.Index(sequence, rule[1]) {
+						swapValues(sequence, rule[0], rule[1])
+					}
+				}
+			}
+		}
+	}
+
+	for _, sequence := range invalidSequences {
+		middle := sequence[len(sequence)/2]
+		middleNum, err := strconv.Atoi(middle)
+		if err != nil {
+			panic(fmt.Sprintf(`Invalid page number: "%s" \n`, middle))
+		}
+
+		invalidAnswer += middleNum
+	}
+
 	fmt.Printf("Answer: %d\n", answer)
-	fmt.Println(validSequences)
+	fmt.Printf("Invalid answer: %d\n", invalidAnswer)
 }
