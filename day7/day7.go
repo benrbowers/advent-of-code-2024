2b3efa66cb1ccb6c2c6f4e25e86f4c82a407ac57
benrbowers <benjambowers@gmail.com>
2024-12-08 07:00:49 -0500

    Attempt at day 7 part 2

--- day7/day7.go
+++ day7/day7.go
@@ -42,7 +42,7 @@ func main() {
 			panic(err)
 		}
 
-		var equationNums []int = []int{}
+		var equationNums []int
 		for _, numString := range strings.Split(parts[1], " ") {
 			num, err := strconv.Atoi(numString)
 			if err != nil {
@@ -52,26 +52,75 @@ func main() {
 			equationNums = append(equationNums, num)
 		}
 
+		var validCombo bool = false
 		n := len(equationNums) - 1 // number of operators
 
-		// Use binary representation, "1010" -> "*+*+"
-		// Loop through all 2^n combinations
-		for i := 0; i < (1 << n); i++ {
-			result := equationNums[0]
+		for x := 0; x < (1 << n); x++ {
+			if validCombo {
+				break
+			}
+			var concatNums []int
+			var concatActive bool = false
 
-			for j := 0; j < n; j++ {
-				if (i & (1 << j)) != 0 {
-					// The j-th bit is set
-					result *= equationNums[j+1]
+			for y := 0; y < n; y++ {
+				if (x & (1 << y)) != 0 {
+					// The y-th bit is set, so concat
+					var num1 string
+					if concatActive {
+						num1 = strconv.Itoa(concatNums[len(concatNums)-1])
+					} else {
+						num1 = strconv.Itoa(equationNums[y])
+					}
+					num2 := strconv.Itoa(equationNums[y+1])
+					newNum, err := strconv.Atoi(num1 + num2)
+					if err != nil {
+						fmt.Printf(`"%s" is not a number:`, num1+num2)
+						panic(err)
+					}
+
+					if concatActive {
+						concatNums[len(concatNums)-1] = newNum
+					} else {
+						concatNums = append(concatNums, newNum)
+					}
+
+					concatActive = true
 				} else {
-					result += equationNums[j+1]
+					concatActive = false
+					concatNums = append(concatNums, equationNums[y])
 				}
 			}
 
-			if result == testNum {
-				answer += testNum
-				break
+			// Use binary representation, "1010" -> "*+*+"
+			// Loop through all 2^n combinations
+			nc := len(concatNums) - 1
+
+			for i := 0; i < (1 << nc); i++ {
+				result := concatNums[0]
+
+				for j := 0; j < nc; j++ {
+					if (i & (1 << j)) != 0 {
+						// The j-th bit is set
+						// fmt.Print("*")
+						result *= concatNums[j+1]
+					} else {
+						// fmt.Print("+")
+						result += concatNums[j+1]
+					}
+				}
+
+				// fmt.Println()
+				if result == testNum {
+					answer += testNum
+					validCombo = true
+					break
+				}
 			}
+
+			// bits := strings.Split(fmt.Sprintf("%b\n", x), "")
+			// slices.Reverse(bits)
+			// fmt.Println(strings.Join(bits, ""))
+			// fmt.Println(concatNums)
 		}
 	}
 
